9e2597b874b72ffc681abbf0bdcb9da716a32da6
Dorian <kidomaval.db@gmail.com>
2021-01-24 19:29:55 +0100

    Bullet tragectoire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import math
 
 res = (640,480)
 
@@ -11,15 +13,38 @@ print(pygame.display.Info())
 ciel = (30,144,255)
 surface.fill(ciel)
 
+bullet_color = (109,7,26)
+bullet = [300,400]
+pygame.draw.circle(surface,bullet_color,bullet,10)
+
 pygame.display.flip()
 
-launched = True
+i= bullet[1]
+t=0
+x0 = bullet[0]
+y0 = bullet[1]
 
+while i<460:
+    time.sleep(.05)
+
+    t += 0.5
+    angle = 30
+    v0 = 75*math.cos(angle)
+    w0 = 75*math.sin(angle)
+
+
+    g = 9.81
+    bullet[0] = x0 + v0*t
+    bullet[1] = 0.5*g*(t**2)+w0*t+y0
+
+    pygame.draw.circle(surface, bullet_color, bullet, 10)
+    pygame.display.flip()
+
+    i = 0.5*g*(t**2)+w0*t+y0
+
+launched = True
 while launched:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             launched = False
             print("exit")
-
-        if event.type == pygame.K_F11:
-            print("test")
