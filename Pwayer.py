6d02852d742b6216d56044931422d02804afe5b6
Rezay <rezaygame@gmail.com>
2021-02-02 19:41:23 +0100

    Pwayer can Jump

--- Pwayer.py
+++ Pwayer.py
@@ -2,15 +2,22 @@ import pygame
 import time
 import math
 
-class Player :
-    def __init__(self, _x, _y, _size, _window):
+
+class Player:
+    def __init__(self, _x, _y, _size, _window, _map):
         self.body = pygame.Rect(_x, _y, _size, _size)
         self.color = (26, 7, 109)
-        self.__speed = 1
-        self.window = _window
+        self.__speed = 3
+        self.__window = _window
+        self.__grounded = False
+        self.__isJumping = False
+        self.__mass = 0.2
+        self.__jumpImpulse = 15
+        self.actualImpulse = 0
+        self.map = _map
 
     def draw(self):
-        pygame.draw.rect(self.window, self.color, self.body)
+        pygame.draw.rect(self.__window, self.color, self.body)
 
     def MovePlayer(self, Direction):
         if Direction == "Left":
@@ -18,5 +25,30 @@ class Player :
         elif Direction == "Right":
             self.body = self.body.move(self.__speed, 0)
 
+    def isJumping(self):
+        return self.__isJumping
 
+    def isGrounded(self):
+        if self.body.collidelistall(self.map):
+            self.__grounded = True
+        else:
+            self.__grounded = False
+        return self.__grounded
 
+    def fall(self, g):
+        if self.isGrounded():
+            self.__isJumping = False
+            return False
+
+        elif self.isJumping():
+            self.actualImpulse += self.__mass * g
+            self.body = self.body.move(0, self.actualImpulse)
+
+        else:
+            self.body = self.body.move(0, self.__mass * g)
+        return True
+
+    def Jump(self):
+        self.actualImpulse = -self.__jumpImpulse
+        self.__isJumping = True
+        self.body = self.body.move(0, self.actualImpulse)
--- test.py
+++ test.py
@@ -13,7 +13,21 @@ pygame.display.set_caption("Worms")
 surface = pygame.display.set_mode(res, pygame.RESIZABLE)
 print(pygame.display.Info())
 
-playerOne = Pwayer.Player(60, 60, 20, surface)
+ground = list()
+ground_color = (155,118,53)
+colide = pygame.Rect(0,440,300,40)
+pygame.draw.rect(surface,ground_color,colide)
+ground.append(colide)
+colide = pygame.Rect(301,440,640,40)
+pygame.draw.rect(surface,ground_color,colide)
+ground.append(colide)
+colision = dict()
+
+g = 9.81
+
+colision["ground"] = ground
+
+playerOne = Pwayer.Player(60, 60, 20, surface, ground)
 
 
 def keyDownAction():
@@ -22,6 +36,8 @@ def keyDownAction():
         playerOne.MovePlayer("Left")
     if keys[pygame.K_RIGHT]:
         playerOne.MovePlayer("Right")
+    if keys[pygame.K_SPACE] and playerOne.isGrounded():
+        playerOne.Jump()
 
 
 ciel = (30, 144, 255)
@@ -32,8 +48,12 @@ while launched:
     clock.tick(60)
     surface.fill(ciel)
 
-    playerOne.draw()
+    for collide in ground:
+        pygame.draw.rect(surface, ground_color, collide)
 
+    playerOne.fall(g)
+
+    playerOne.draw()
     pygame.display.flip()
 
     keyDownAction()
