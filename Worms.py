037df91803b3f8dc37728e918d70492db150556b
Dorian <kidomaval.db@gmail.com>
2021-02-18 18:29:16 +0100

    modif

--- Worms.py
+++ Worms.py
@@ -39,6 +39,10 @@ vitesse = 0
 x_souris = 0
 y_souris = 0
 
+weapon = 0
+debug_mode = 0
+wind_speed = 10
+
 colision["ground"] = ground
 
 playerOne = Pwayer.Player(60, 60, 20, surface, ground)
@@ -65,11 +69,20 @@ while launched:
     clock.tick(60)
     surface.fill(ciel)
 
+
     text_fps = arial_font_fps.render(f"{clock.get_fps():.2f} FPS", True, fps_color)
     angle_number = arial_font_fps.render(f"{angle_degree} °", True, fps_color)
     AB_number = arial_font_fps.render(f"AB = {AB}", True, traj_color)
     AC_number = arial_font_fps.render(f"AC = {AC}", True, traj_color)
     vitesse_number = arial_font_fps.render(f"vitesse = {vitesse}", True, traj_color)
+
+    if weapon==0:
+        weapon_name="grenade"
+    elif weapon==1:
+        weapon_name="rocket"
+
+    weapon_render = arial_font_fps.render(f"Weapon : {weapon_name}", True,traj_color)
+    wind_render = arial_font_fps.render(f"Wind speed : {wind_speed}", True,traj_color)
 
     AB = math.sqrt(((x_souris - (playerOne.body.centerx)) ** 2) + ((y_souris - (playerOne.body.centery)) ** 2))
     AC = x_souris - (playerOne.body.centerx)
@@ -89,13 +102,14 @@ while launched:
         else:
             angle_degree = round(angle_radian * 180 / math.pi, 0)
 
-    pygame.draw.line(surface, traj_color, [playerOne.body.centerx, playerOne.body.centery],[x_souris, y_souris])
-
-    surface.blit(text_fps, [10, 10])
-    surface.blit(angle_number, [500, 10])
-    surface.blit(AB_number, [500, 20])
-    surface.blit(AC_number, [500, 30])
-    surface.blit(vitesse_number, [500, 40])
+    if debug_mode == 1:
+        surface.blit(text_fps, [10, 10])
+        surface.blit(angle_number, [500, 10])
+        surface.blit(AB_number, [500, 20])
+        surface.blit(AC_number, [500, 30])
+        surface.blit(vitesse_number, [500, 40])
+        surface.blit(weapon_render, [500, 50])
+        surface.blit(wind_render, [500, 60])
 
     x0 = playerOne.body.centerx
     y0 = playerOne.body.centery
@@ -131,47 +145,23 @@ while launched:
             print("exit")
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                #grenade.play()
-
-                bullet.x=playerOne.body.x
-                bullet.y=playerOne.body.y
-                t = 0
-                x0 = playerOne.body.x
-                y0 = playerOne.body.y
-
-                while not colide.colliderect(bullet) and bullet.x<1260 and bullet.y<940:
-                    time.sleep(.05)
-                    surface.fill(ciel)
-
-                    t += 0.5
-                    if(y_souris>playerOne.body.y):
-                        angle = angle_radian
-                    else:
-                        angle = -angle_radian
-
-                    v0 = vitesse * math.cos(angle)
-                    w0 = vitesse * math.sin(angle)
-
-                    bullet[0] = x0 + v0 * t
-                    bullet[1] = 0.5 * g * (t ** 2) + w0 * t + y0
-
-                    pygame.draw.rect(surface, bullet_color, bullet)
-                    playerOne.draw()
-                    pygame.draw.rect(surface, ground_color, collide)
-
-                    pygame.draw.line(surface, bullet_color, [playerOne.body.centerx, playerOne.body.centery], [x_souris, y_souris])
-
-                    pygame.display.flip()
-
-                    i = 0.5 * g * (t ** 2) + w0 * t + y0
-
-                #explosion.play()
-            elif event.key == pygame.K_r:
+            if event.key == pygame.K_r:
                 if traj_status == 0:
                     traj_status = 1
                 elif traj_status == 1:
                     traj_status = 0
+
+            elif event.key == pygame.K_TAB:
+                if weapon==0:
+                    weapon=1
+                elif weapon==1:
+                    weapon=0
+
+            elif event.key == pygame.K_F3:
+                if debug_mode==0:
+                    debug_mode=1
+                elif debug_mode==1:
+                    debug_mode=0
 
         elif event.type == pygame.MOUSEMOTION:
             #print("{}".format(event.pos))
@@ -180,15 +170,17 @@ while launched:
             x_souris = event.pos[0]
             y_souris = event.pos[1]
 
-            pygame.draw.line(surface, traj_color, [playerOne.body.centerx, playerOne.body.centery], [event.pos[0],event.pos[1]])
             playerOne.draw()
             pygame.draw.rect(surface, ground_color, collide)
 
-            surface.blit(text_fps, [10, 10])
-            surface.blit(AB_number, [500, 20])
-            surface.blit(AC_number, [500, 30])
-            surface.blit(vitesse_number, [500, 40])
-            surface.blit(angle_number, [500, 10])
+            if debug_mode == 1:
+                surface.blit(text_fps, [10, 10])
+                surface.blit(AB_number, [500, 20])
+                surface.blit(AC_number, [500, 30])
+                surface.blit(vitesse_number, [500, 40])
+                surface.blit(angle_number, [500, 10])
+                surface.blit(weapon_render, [500, 50])
+                surface.blit(wind_render, [500, 60])
 
             x0 = playerOne.body.centerx
             y0 = playerOne.body.centery
@@ -212,3 +204,41 @@ while launched:
             #surface.blit(text_fps, [10, 10])
 
             pygame.display.flip()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # grenade.play()
+
+            bullet.x = playerOne.body.x
+            bullet.y = playerOne.body.y
+            t = 0
+            x0 = playerOne.body.x
+            y0 = playerOne.body.y
+
+            while not colide.colliderect(bullet) and bullet.x < 1260 and bullet.y < 940:
+                time.sleep(.05)
+                surface.fill(ciel)
+
+                t += 0.5
+                if (y_souris > playerOne.body.y):
+                    angle = angle_radian
+                else:
+                    angle = -angle_radian
+
+                v0 = vitesse * math.cos(angle)
+                w0 = vitesse * math.sin(angle)
+
+                if weapon == 0:
+                    bullet[0] = x0 + v0 * t
+                    bullet[1] = 0.5 * g * (t ** 2) + w0 * t + y0
+
+                elif weapon == 1:
+                    bullet[0] = x0 + ((v0*wind_speed) * (1))
+                    bullet[1] = y0 + (((w0/wind_speed) + (g/wind_speed**2)) * (1)) - ((g*t)/wind_speed)
+
+                pygame.draw.rect(surface, bullet_color, bullet)
+                playerOne.draw()
+                pygame.draw.rect(surface, ground_color, collide)
+
+                pygame.display.flip()
+
+                i = 0.5 * g * (t ** 2) + w0 * t + y0
